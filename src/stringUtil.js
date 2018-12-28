9486358a2fd9e831f89f1a4112952d626257e023
Ballabh Heera <44017701+heerajoshi@users.noreply.github.com>
2018-12-28 10:25:01 +0530

    Rename the args of add function

--- src/stringUtil.js
+++ src/stringUtil.js
@@ -1,6 +1,6 @@
 const TAB = "\t";
 const NEWLINE = "\n";
 const isNotEmpty = word => word != "";
-const add = (a, b) => a + b;
+const add = (firstNum, secondNum) => firstNum + secondNum;
 
 module.exports = { TAB, NEWLINE, isNotEmpty, add };
